d9b28b140a827eec916890e1a4cc688ceeb2d2ae
Axionauta <daniln2209@gmail.com>
2022-12-04 13:22:55 -0300

    feat: adiciona atributos

--- src/main/java/br/com/mildevs/model/Condutor.java
+++ src/main/java/br/com/mildevs/model/Condutor.java
@@ -1,5 +1,21 @@
 package br.com.mildevs.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 public class Condutor {
-  
+  @Id
+  @Column(name = "numero_cnh")
+  private int numeroCnh;
+  @Column(name = "data_emissao_cnh")
+  private LocalDate dataEmissaoCnh;
+  @Column(name = "orgao_emissor", length = 5)
+  private String orgaoEmissor;
+  private int pontuacao;
+  @OneToMany(mappedBy = "condutor")
+  private List<Veiculo> veiculos;
 }
