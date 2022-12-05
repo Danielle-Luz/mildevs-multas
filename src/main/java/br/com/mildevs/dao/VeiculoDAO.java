630606dac7785652206b092fff737f8c5f388111
Axionauta <daniln2209@gmail.com>
2022-12-04 22:28:18 -0300

    feat: adiciona método para criar veículo

--- src/main/java/br/com/mildevs/dao/VeiculoDAO.java
+++ src/main/java/br/com/mildevs/dao/VeiculoDAO.java
@@ -1,5 +1,22 @@
 package br.com.mildevs.dao;
 
+import br.com.mildevs.model.Veiculo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class VeiculoDAO {
-  
+  private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mildevs-multas");
+
+  public static void inserirVeiculo (Veiculo novoVeiculo) {
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    entityManager.getTransaction().begin();
+
+    entityManager.persist(novoVeiculo);
+
+    entityManager.getTransaction().commit();
+
+    entityManager.close();
+  }
 }
