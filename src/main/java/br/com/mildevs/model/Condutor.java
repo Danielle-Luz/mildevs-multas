8dc84558fece72ba77a8228f88d16b8c27674044
Axionauta <daniln2209@gmail.com>
2022-12-04 22:54:13 -0300

    feat: corrige endentação do condutor

--- src/main/java/br/com/mildevs/model/Condutor.java
+++ src/main/java/br/com/mildevs/model/Condutor.java
@@ -1,71 +1,72 @@
 package br.com.mildevs.model;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 public class Condutor {
-  @Id
-  @Column(name = "numero_cnh")
-  private int numeroCnh;
-  @Column(name = "data_emissao_cnh")
-  private LocalDate dataEmissaoCnh;
-  @Column(name = "orgao_emissor", length = 5, nullable = false)
-  private String orgaoEmissor;
-  @Column(nullable = false)
-  private int pontuacao;
-  @OneToMany(mappedBy = "condutor", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
-  private List<Veiculo> veiculos;
 
-  public Condutor() {
+    @Id
+    @Column(name = "numero_cnh")
+    private int numeroCnh;
 
-  }
+    @Column(name = "data_emissao_cnh")
+    private LocalDate dataEmissaoCnh;
 
-  public int getNumeroCnh() {
-    return numeroCnh;
-  }
+    @Column(name = "orgao_emissor", length = 5, nullable = false)
+    private String orgaoEmissor;
 
-  public void setNumeroCnh(int numeroCnh) {
-    this.numeroCnh = numeroCnh;
-  }
+    @Column(nullable = false)
+    private int pontuacao;
 
-  public LocalDate getDataEmissaoCnh() {
-    return dataEmissaoCnh;
-  }
+    @OneToMany(mappedBy = "condutor", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private List<Veiculo> veiculos;
 
-  public void setDataEmissaoCnh(LocalDate dataEmissaoCnh) {
-    this.dataEmissaoCnh = dataEmissaoCnh;
-  }
+    public Condutor() {}
 
-  public String getOrgaoEmissor() {
-    return orgaoEmissor;
-  }
+    public int getNumeroCnh() {
+        return numeroCnh;
+    }
 
-  public void setOrgaoEmissor(String orgaoEmissor) {
-    this.orgaoEmissor = orgaoEmissor;
-  }
+    public void setNumeroCnh(int numeroCnh) {
+        this.numeroCnh = numeroCnh;
+    }
 
-  public int getPontuacao() {
-    return pontuacao;
-  }
+    public LocalDate getDataEmissaoCnh() {
+        return dataEmissaoCnh;
+    }
 
-  public void setPontuacao(int pontuacao) {
-    this.pontuacao = pontuacao;
-  }
+    public void setDataEmissaoCnh(LocalDate dataEmissaoCnh) {
+        this.dataEmissaoCnh = dataEmissaoCnh;
+    }
 
-  public List<Veiculo> getVeiculos() {
-    return veiculos;
-  }
+    public String getOrgaoEmissor() {
+        return orgaoEmissor;
+    }
 
-  public void setVeiculos(List<Veiculo> veiculos) {
-    this.veiculos = veiculos;
-  }
-  
+    public void setOrgaoEmissor(String orgaoEmissor) {
+        this.orgaoEmissor = orgaoEmissor;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public List<Veiculo> getVeiculos() {
+        return veiculos;
+    }
+
+    public void setVeiculos(List<Veiculo> veiculos) {
+        this.veiculos = veiculos;
+    }
 }
