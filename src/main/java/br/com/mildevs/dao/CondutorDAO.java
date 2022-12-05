0f84c11ebc0f51cb68653098e1721057a0bd43ab
Axionauta <daniln2209@gmail.com>
2022-12-04 22:00:13 -0300

    feat: adiciona método de criar condutor

--- src/main/java/br/com/mildevs/dao/CondutorDAO.java
+++ src/main/java/br/com/mildevs/dao/CondutorDAO.java
@@ -1,5 +1,18 @@
 package br.com.mildevs.dao;
 
+import br.com.mildevs.model.Condutor;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class CondutorDAO {
-  
+  private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mildevs-multas");
+
+  public static void inserirCondutor (Condutor novoCondutor) {
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    entityManager.persist(novoCondutor);
+
+    entityManager.close();
+  }
 }
