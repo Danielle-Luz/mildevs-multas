603fefde08685aa595c4c57d3a44c22ddf4c7e76
Axionauta <daniln2209@gmail.com>
2022-12-06 17:39:00 -0300

    feat: adiciona método pra exibir menu genérico

--- src/main/java/br/com/mildevs/controller/Menu.java
+++ src/main/java/br/com/mildevs/controller/Menu.java
@@ -1,5 +1,23 @@
 package br.com.mildevs.controller;
 
+import br.com.mildevs.Scanner.lerDados;
+
 public class Menu {
-  
+  public static void exibirMenuGenerico() {
+    String textoMenuGenerico = "O que você deseja gerenciar?\n1- Condutor\n2- Multas\n 3-Veiculos";
+
+    int opcao = lerDados.lerIntComLimites(textoMenuGenerico, 1, 3);
+
+    switch(opcao) {
+      case 1:
+        
+        break;
+      case 2:
+        
+        break;
+      case 3:
+        
+        break;
+    }
+  }
 }
