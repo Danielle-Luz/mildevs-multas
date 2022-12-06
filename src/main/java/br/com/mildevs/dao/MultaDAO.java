e161bc891db688ba20b15a829b1fd8185a8b3e7f
Axionauta <daniln2209@gmail.com>
2022-12-06 17:08:58 -0300

    feat: adiciona método de inserir multa

--- src/main/java/br/com/mildevs/dao/MultaDAO.java
+++ src/main/java/br/com/mildevs/dao/MultaDAO.java
@@ -1,5 +1,22 @@
 package br.com.mildevs.dao;
 
+import br.com.mildevs.model.Multa;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class MultaDAO {
-  
+  static private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mildevs-multas");
+
+  public static void inserirMulta(Multa novaMulta) {
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    entityManager.getTransaction().begin();
+
+    entityManager.persist(novaMulta);
+
+    entityManager.getTransaction().commit();
+
+    entityManager.close();
+  }
 }
